d6fa293c525561582ad2e5a9334574142ae2c4f3
Phat <damphat@gmail.com>
2020-07-14 02:55:03 +0700

    it works

---
+++ Sigobase.Tests/SigoTests.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace Sigobase.Tests {
+    public class SigoTests {
+
+        [Fact]
+        public void MergeTest() {
+            ISigo user = Sigo.Create(3, "name", "phat");
+            ISigo addAge = Sigo.Create(0, "age", 30);
+
+            ISigo ret = Sigo.Merge(user, addAge);
+
+            Assert.StrictEqual(Sigo.Create(3, "name", "phat", "age", 30), ret);
+        }
+    }
+}
---
+++ Sigobase/ISigo.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Sigobase {
+    public interface ISigo {
+        int Flags { get; }
+        object Data { get; }
+
+        ISigo Get1(string key);
+        ISigo Set1(string key, ISigo value);
+        IEnumerable<string> Keys { get; }
+
+        ISigo Freeze();
+    }
+}
