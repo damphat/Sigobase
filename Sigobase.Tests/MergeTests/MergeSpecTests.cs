f80a1a751299864f7a4523f31ebf06b6e67b58bb
Phat <damphat@gmail.com>
2020-07-20 16:30:57 +0700

    MergeSpecTests, which shows the bug in MergeSpec

---
+++ Sigobase.Tests/MergeTests/MergeSpecTests.cs
@@ -0,0 +1,31 @@
+﻿using Sigobase.Database;
+using Sigobase.Implements;
+using Xunit;
+
+namespace Sigobase.Tests.MergeTests {
+    public class MergeSpecTests {
+        [Fact]
+        public void Try_to_return_part_of_a() {
+            var a = Sigo.Create(7,
+                "x/name", "Phat"
+            ).Freeze();
+
+            var b = Sigo.Create(7,
+                "x/name", "Phat",
+                "y", "y"
+            ).Freeze();
+
+            var r = ImplMergeSpec.Merge(a, b);
+            
+            // r equal b...
+            Assert.Equal(r, b);
+
+            // ...but not same b...
+            Assert.NotSame(r, b);
+
+            // ...because there is r[x] == a[x]
+            Assert.Equal(r["x"], a["x"]);
+            Assert.Same(r["x"], a["x"]);
+        }
+    }
+}
