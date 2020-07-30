8d919a708e91341a1bf20c77288862fc3ea5ead3
Phat <damphat@gmail.com>
2020-07-30 18:05:38 +0700

    add NothingSchema

---
+++ Sigobase.Generator/Schemas/NothingSchema.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Sigobase.Database;
+
+namespace Sigobase.Generator.Schemas {
+    internal class NothingSchema : Schema {
+        public override IEnumerable<ISigo> Values(Options options) {
+            yield break;
+        }
+
+        public override int Count() {
+            return 0;
+        }
+    }
+}
