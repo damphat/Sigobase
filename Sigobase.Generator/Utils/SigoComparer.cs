a9cc067dbb359323afbf8e2680c801cd6990c7d1
Phat <damphat@gmail.com>
2020-07-30 22:56:49 +0700

    improve SigoComparer - null cases - using() statements for netstandard2.0 - child-order does master - cleanup

--- Sigobase.Generator/Utils/SigoComparer.cs
+++ Sigobase.Generator/Utils/SigoComparer.cs
@@ -5,6 +5,7 @@ using Sigobase.Database;
 namespace Sigobase.Generator.Utils {
     // Fixed Comparer<object>.Default.Compare(x.Data = "2", y.Data = 1);
     // Fixed method overloading + inheritance
+    // child orders master
     internal class SigoComparer : Comparer<ISigo> {
         private static readonly Type[] Types = new Type[] {
             typeof(bool),
@@ -17,40 +18,69 @@ namespace Sigobase.Generator.Utils {
         }
 
         private static int CompareObject(object x, object y) {
-            if (ReferenceEquals(x, y)) return 0;
-            if (y == null) return 1; // everything > null
+            if (ReferenceEquals(x, y)) {
+                return 0;
+            }
+
+            if (x == null) {
+                return -1;  // null < any
+            }
+
+            if (y == null) {
+                return 1;   // any > null
+            }
 
             var tx = x.GetType();
             var ty = y.GetType();
 
-            if (tx == ty) return Comparer<object>.Default.Compare(x, y);
+            if (tx == ty) {
+                return Comparer<object>.Default.Compare(x, y);
+            }
+
             return CompareType(tx, ty);
         }
 
-        private int CompareChildren(ISigo x, ISigo y) {
-            using var xe = x.GetEnumerator();
-            using var ye = y.GetEnumerator();
-            while (xe.MoveNext() && ye.MoveNext()) {
-                var delta = string.CompareOrdinal(xe.Current.Key, ye.Current.Key);
-                if (delta != 0) return delta;
-                delta = Compare(xe.Current.Value, ye.Current.Value);
-                if (delta != 0) return delta;
-            }
+        private int CompareChildrenWithOrder(ISigo x, ISigo y) {
+            using (var xe = x.GetEnumerator()) {
+                using (var ye = y.GetEnumerator()) {
+                    while (xe.MoveNext() && ye.MoveNext()) {
+                        var delta = string.CompareOrdinal(xe.Current.Key, ye.Current.Key);
+                        if (delta != 0) {
+                            return delta;
+                        }
 
-            return 0;
+                        delta = Compare(xe.Current.Value, ye.Current.Value);
+                        if (delta != 0) {
+                            return delta;
+                        }
+                    }
+
+                    return 0;
+                }
+            }
         }
 
         public override int Compare(ISigo x, ISigo y) {
-            if (ReferenceEquals(x, y)) return 0;
+            if (ReferenceEquals(x, y)) {
+                return 0;
+            }
+
+            if (x == null) {
+                return -1;  // null < any
+            }
+
+            if (y == null) {
+                return 1;   // any > null
+            }
+
+            // include neutrons
             if (x.Flags != y.Flags) {
                 return x.Flags - y.Flags;
             }
 
-            if (x.IsLeaf()) {
-                return CompareObject(x.Data, y.Data);
-            } else {
-                return CompareChildren(x, y);
-            }
+            return x.IsLeaf()
+                ? CompareObject(x.Data, y.Data)
+                : CompareChildrenWithOrder(x, y);
         }
     }
 }
