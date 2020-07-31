254891bf2c958b669d37d8945810c4f48f1d94c6
Phat <damphat@gmail.com>
2020-07-31 18:22:09 +0700

    add ParserTests

---
+++ Sigobase.Language.Tests/ParserTests.cs
@@ -0,0 +1,15 @@
+﻿using Sigobase.Database;
+using Sigobase.Language.Lang;
+using Xunit;
+
+namespace Sigobase.Language.Tests {
+    public class ParserTests {
+        [Fact]
+        public void ObjectTest() {
+            var result = new Parser("{a: {b:1, c:1}}").Parse();
+            var expected = Sigo.Create(3, "b", 1, "c", 1);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
