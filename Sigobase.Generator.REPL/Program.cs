6ed775a07579d68745a973900fc92f7d8a0f7a24
Phat <damphat@gmail.com>
2020-08-02 02:58:03 +0700

    readable, easier to use

--- Sigobase.Generator.REPL/Program.cs
+++ Sigobase.Generator.REPL/Program.cs
@@ -1,85 +1,73 @@
-﻿using System;
-using Sigobase.Database;
-using Sigobase.Generator.Lang;
-using Sigobase.Generator.Schemas;
+﻿using Sigobase.Database;
+using Sigobase.Utils;
+using System;
+using System.Collections.Generic;
 
 namespace Sigobase.Generator.REPL {
     internal class Program {
-        private static void PeekableLexerTest() {
-            var lex = new PeekableLexer("0 1 2 3 4 5 6", -1, 2);
-            while (true) {
-                try {
-                    var cmd = Console.ReadLine();
-                    if (string.IsNullOrEmpty(cmd)) {
-                        continue;
-                    }
 
-                    switch (cmd[0]) {
-                        case 'm':
-                            lex.Move(int.Parse(cmd.Substring(1)));
-                            break;
-                        case 'p':
-                            lex.Peek(int.Parse(cmd.Substring(1)));
-                            break;
-                    }
-
-                    Console.WriteLine(lex);
-                } catch (Exception e) {
-                    Console.Error.WriteLine(e.Message);
-                }
+        private static void Help() {
+            Console.WriteLine("Current Schemas:");
+            foreach (var e in SigoSchema.SchemaDict) {
+                Console.WriteLine($"\t{e.Key}\t=\t{e.Value}");
             }
+
+            Console.WriteLine("  syntax: <name> '=' <schemaDefinition>  // to define a schema");
+            Console.WriteLine("  syntax: <schemaDefinition> | <schemaName> // to list all possible values");
+            Console.WriteLine();
+        }
+
+        private static void GenerateAllPossibleValues(SigoSchema schema) {
+            Console.WriteLine($"About {schema.Count()} items in {schema}");
+            Console.WriteLine("--------------------");
+            var count = 0;
+            var values = schema.Generate(GenerateOptions.UniqueSorted);
+            foreach (var value in values) {
+                Console.WriteLine(value.ToString(Writer.Default));
+                count++;
+            }
+            Console.WriteLine($"\t{count}/{schema.Count()} items listed");
         }
 
         private static void Main() {
-            var example = @"
-                age  = 20 | 40; 
-                name = 'An' | 'Bao'; 
-                user = {name: name, age?: age}
-                user = {name, age?}
-                all  = user | name | age | {}
-                ";
+            var statements = new List<string> {
+                "    string = 'usd'|'eur'",
+                "    number = 1|2|3",
+                "    bool = true|false",
+                "    account = {kind: string, amount?: number}"
+            };
 
-            Console.WriteLine(@"schema examples:");
-            Console.WriteLine(example);
+            // eval the initial statements
+            foreach (var statement in statements) {
+                SigoSchema.Parse(statement);
+            }
 
-            new Parser(example).Parse();
+            Help();
 
             while (true) {
-                Console.Write('>');
+                Console.Write("schema>");
                 var src = Console.ReadLine();
-                if (string.IsNullOrEmpty(src)) {
-                    continue;
-                }
-
                 switch (src) {
+                    case null:
+                    case "":
+                        continue;
                     case "?":
                     case "help": {
-                        foreach (var e in Schema.SchemaDict) {
-                            Console.WriteLine($"{e.Key} = {e.Value}");
-                        }
-
+                        Help();
                         continue;
                     }
                     case "exit":
                     case "quit":
                         return;
                     default: {
-                        var parser = new Parser(src);
 
                         try {
-                            var schema = parser.Parse();
-                            Console.WriteLine($"About {schema.Count()} items in {schema}");
-                            Console.WriteLine("--------------------");
-                            var count = 0;
-                            var values = schema.Values(Options.UniqueSorted);
-                            foreach (var value in values) {
-                                Console.WriteLine(value.ToString(0));
-                                count++;
-                            }
+                            var schema = SigoSchema.Parse(src);
+                            if(schema.Count() == 0) continue;
 
-                            Console.WriteLine($"{count}/{schema.Count()} items listed");
+                           GenerateAllPossibleValues(schema);
                         } catch (Exception e) {
-                            Console.Error.WriteLine(e);
+                            Console.Error.WriteLine(e.Message);
                         }
 
                         break;
