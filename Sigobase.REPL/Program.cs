8dddbc065aacb7bb748d37bc6a6d232c90a1b314
Phat <damphat@gmail.com>
2020-08-04 15:07:15 +0700

    sigobase repl, which is call Sigo.Parse(cmd) and print the result

--- Sigobase.REPL/Program.cs
+++ Sigobase.REPL/Program.cs
@@ -1,17 +1,31 @@
 ﻿using System;
 using Sigobase.Database;
+using Sigobase.Utils;
 
 namespace Sigobase.REPL {
     internal class Program {
-        private static void Main(string[] args) {
-            var user = Sigo.Create(1,
-                "name", Sigo.Create(1,
-                    "first", "Phat",
-                    "last", "Dam"),
-                "age", 40);
+        private static readonly Writer DefaultWriter = Writer.Pretty;
 
-            Console.WriteLine(user);
-            Console.Read();
+        private static void Main(string[] args) {
+            Console.WriteLine("Welcome to sigo REPL");
+            while (true) {
+                Console.Write("sigo> ");
+                var src = Console.ReadLine();
+                if (src == "exit") {
+                    return;
+                }
+                if (src == "") continue;
+                if (src == "cls" || src == "clear") {
+                    Console.Clear();
+                    continue;
+                }
+
+                try {
+                    Console.WriteLine(Sigo.Parse(src).ToString(DefaultWriter));
+                } catch (Exception e) {
+                    Console.Error.WriteLine(e.Message);
+                }
+            }
         }
     }
 }
