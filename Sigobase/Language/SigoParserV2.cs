3dd62a38a0386f6e6160b753fde7b7315fbf0ae0
Phat <damphat@gmail.com>
2020-08-08 21:32:27 +0700

    done without path and context variable - already support expression "-(1+2)*3" - not support path {user/name: 'phat'} - not support context varibale "x = 10"

--- Sigobase/Language/SigoParserV2.cs
+++ Sigobase/Language/SigoParserV2.cs
@@ -1,15 +1,15 @@
 ﻿using System;
-using System.Diagnostics.CodeAnalysis;
+using System.Collections.Generic;
 using Sigobase.Database;
+using Sigobase.Language.Utils;
 
 namespace Sigobase.Language {
     public class SigoParserV2 : SigoParser {
         private readonly PeekableLexer lexer;
         private Token t;
 
-        public SigoParserV2(string src) 
-        {
-            lexer = new PeekableLexer(src,0,1);
+        public SigoParserV2(string src) {
+            lexer = new PeekableLexer(src, 0, 1);
             t = lexer.Peek(0);
         }
 
@@ -26,6 +26,193 @@ namespace Sigobase.Language {
             return t.Kind == Kind.Eof;
         }
 
+        void Require(Kind kind) {
+            if (t.Kind != kind) {
+                Next();
+                throw Expect(kind.ToString());
+            }
+
+            Next();
+        }
+
+        Kind ReadKind() {
+            var ret = t.Kind;
+            Next();
+            return ret;
+        }
+
+        object Eat(object v) {
+            Next();
+            return v;
+        }
+
+
+        public object Hang() {
+            object Parens() {
+                Next(); // Require(Kind.OpenBracket);
+                var expr = Expr();
+                Require(Kind.CloseParens);
+                return expr;
+            }
+
+            switch (t.Kind) {
+                case Kind.Minus:
+                case Kind.Plus: return Operators.Unary(ReadKind(), Hang());
+                case Kind.Number: return Eat(t.Value);
+                case Kind.String: return Eat(t.Value);
+                case Kind.Identifier:
+                    switch (t.Raw) {
+                        case "true": return Eat(true);
+                        case "false": return Eat(false);
+                        case "Infinity": return Eat(double.PositiveInfinity);
+                        case "NaN": return Eat(double.NaN);
+                        default: return Eat(t.Raw);
+                    }
+                case Kind.OpenParens: return Parens();
+                case Kind.OpenBracket: return ParseArray();
+                case Kind.Open: return ParseObject();
+                default:
+                    throw Expect("hang");
+            }
+        }
+
+        bool Sep() {
+            if (t.Kind == Kind.Comma || t.Kind == Kind.SemiColon) {
+                Next();
+                return true;
+            }
+
+            return t.Separator > 0;
+        }
+
+
+        private object ParseArray() {
+            bool Close() {
+                if (t.Kind == Kind.CloseBracket) {
+                    Next();
+                    return true;
+                }
+
+                return false;
+            }
+
+            Next();
+            var list = new List<object>();
+            if (Close()) return list;
+
+            while (true) {
+                list.Add(Expr());
+                if (Close()) return list;
+                if (Sep()) {
+                    if (Close()) return list;
+                    else { /*loop*/}
+                } else {
+                    throw Expect("','");
+                }
+            }
+        }
+
+        private string ReadKey() {
+            switch (t.Kind) {
+                case Kind.String: return (string)Eat(t.Value);
+                case Kind.Identifier: return (string)Eat(t.Raw);
+                case Kind.Number: return Operators.ToStr(Eat(t.Value));
+                default:
+                    throw Expect("key");
+            }
+        }
+        private ISigo ReadKeyValue(ISigo obj) {
+            var key = ReadKey();
+            Require(Kind.Colon);
+            var value = Expr();
+            return obj.Set1(key, Sigo.From(value));
+        }
+
+        private object ParseObject() {
+            ISigo ParseFlag() {
+                var raw = t.Raw;
+                var k1 = lexer.Peek(1).Kind;
+                if (raw.Length > 0 && raw[0] >= '0' && raw[0] <= '7' && (k1 != Kind.Colon && k1 != Kind.Div)) {
+                    Next();
+                    return Sigo.Create(raw[0] - '0');
+                }
+
+                return null;
+            }
+
+            bool Close() {
+                if (t.Kind == Kind.Close) {
+                    Next();
+                    return true;
+                }
+
+                return false;
+            }
+
+
+            Next();
+            var obj = ParseFlag();
+            if (obj == null) {
+                obj = Sigo.Create(3);
+                if (Close()) return obj;
+                else {
+                    /*loop*/
+                }
+            } else {
+                if (Sep()) {
+                    if (Close()) return obj;
+                    else { /*loop*/ }
+                } else {
+                    if (Close()) return obj;
+                    else {
+                        throw Expect("','");
+                    }
+                }
+
+            }
+
+            while (true) {
+                obj = ReadKeyValue(obj);
+                if (Close()) return obj;
+                if (Sep()) {
+                    if (Close()) return obj;
+                    else { /*loop*/}
+                } else {
+                    throw Expect("','");
+                }
+
+            }
+
+        }
+
+        public object Tich() {
+            var hang = Hang();
+            while (true) {
+                switch (t.Kind) {
+                    case Kind.Mul:
+                    case Kind.Div:
+                        hang = Operators.Binary(ReadKind(), hang, Hang());
+                        break;
+                    default:
+                        return hang;
+                }
+            }
+        }
+
+        public object Tong() {
+            var tong = Tich();
+            while (true) {
+                switch (t.Kind) {
+                    case Kind.Plus:
+                    case Kind.Minus:
+                        tong = Operators.Binary(ReadKind(), tong, Tich());
+                        break;
+                    default:
+                        return tong;
+                }
+            }
+        }
+
         /// <summary>
         /// TODO
         /// expr : tong
@@ -37,18 +224,7 @@ namespace Sigobase.Language {
         ///      | identifier
         /// </summary>
         public object Expr() {
-            if (t.Kind == Kind.Number) {
-                var ret = t.Value;
-                Next();
-                return ret;
-            }
-
-            if (t.Kind == Kind.Identifier) {
-                var ret = t.Raw;
-                Next();
-                return ret;
-            }
-            throw Expect("expression");
+            return Tong();
         }
 
         /// <summary>
@@ -61,7 +237,7 @@ namespace Sigobase.Language {
         ///        | expr Expect(';')
         /// </summary>
         private object Program() {
-            bool SemiColon() {
+            bool ExprSep() {
                 if (t.Kind == Kind.SemiColon) {
                     Next();
                     return true;
@@ -69,6 +245,7 @@ namespace Sigobase.Language {
 
                 return false;
             }
+
             object last = Sigo.Create(0);
             if (Eof()) return last;
 
@@ -76,9 +253,11 @@ namespace Sigobase.Language {
             while (true) {
                 last = Expr();
                 if (Eof()) return last;
-                if (SemiColon()) {
+                if (ExprSep()) {
                     if (Eof()) return last;
-                    else { /*loop*/ }
+                    else {
+                        /*loop*/
+                    }
                 } else {
                     throw Expect("';'");
                 }
